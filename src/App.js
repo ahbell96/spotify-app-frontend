3e7f14033f0db77f935a3f0e0b1aeb7b088fa757
Andrew Bell <andrew_h_bell@outlook.com>
2021-07-16 15:17:13 +0100

    converted app to class component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {Component} from 'react';
 import logo from "./logo.svg";
 import "@fontsource/roboto";
 import { Grid, Button, Typography } from "@material-ui/core";
@@ -6,26 +7,22 @@ import "./App.css";
 import {faArrowAltCircleLeft, faArrowAltCircleRight, faPlayCircle} from '@fortawesome/free-regular-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-function App() {
-  return (
-    <Grid container className='App container-mx-height' justifyContent='center' alignItems="center">
-      <Grid item alignContent="center" alignItems="center" justifyContent="center" style={{display: 'flex'}}>
-      <Grid item><FontAwesomeIcon icon={faArrowAltCircleLeft} style={{width: 80, height: 80}}/></Grid>
-      <Grid item>
-        <FontAwesomeIcon icon={faPlayCircle} style={{width: 250, height: 250, margin: 40}}></FontAwesomeIcon>
-      </Grid>
-      {/* 
-        <Grid
-          item
-          style={{ width: 300, height: 300, backgroundColor: "red", margin: 40 }} className='br-circle'
-        >
-          <Grid></Grid>
-        </Grid>
-        */}
-        <Grid item><FontAwesomeIcon icon={faArrowAltCircleRight} style={{width: 80, height: 80}}/></Grid>
-      </Grid>
-    </Grid>
-  );
-}
+export default class App extends Component {
+  state = {
 
-export default App;
+  }
+
+  render() {
+    return (
+      <Grid container className='App container-mx-height' justifyContent='center' alignItems="center">
+        <Grid item alignContent="center" alignItems="center" justifyContent="center" style={{display: 'flex'}}>
+        <Grid item><FontAwesomeIcon icon={faArrowAltCircleLeft} style={{width: 80, height: 80}}/></Grid>
+        <Grid item>
+          <FontAwesomeIcon icon={faPlayCircle} style={{width: 250, height: 250, margin: 40}}></FontAwesomeIcon>
+        </Grid>
+          <Grid item><FontAwesomeIcon icon={faArrowAltCircleRight} style={{width: 80, height: 80}}/></Grid>
+        </Grid>
+      </Grid>
+    );
+  }
+}
