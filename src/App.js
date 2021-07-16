3e25a3581c08d73d5310f63c2dd005e4baa88f9a
Andrew Bell <andrew_h_bell@outlook.com>
2021-07-16 15:14:31 +0100

    addition of font awesome and an initial structure for the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "@fontsource/roboto";
+import { Grid, Button, Typography } from "@material-ui/core";
+import "./App.css";
+
+import {faArrowAltCircleLeft, faArrowAltCircleRight, faPlayCircle} from '@fortawesome/free-regular-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grid container className='App container-mx-height' justifyContent='center' alignItems="center">
+      <Grid item alignContent="center" alignItems="center" justifyContent="center" style={{display: 'flex'}}>
+      <Grid item><FontAwesomeIcon icon={faArrowAltCircleLeft} style={{width: 80, height: 80}}/></Grid>
+      <Grid item>
+        <FontAwesomeIcon icon={faPlayCircle} style={{width: 250, height: 250, margin: 40}}></FontAwesomeIcon>
+      </Grid>
+      {/* 
+        <Grid
+          item
+          style={{ width: 300, height: 300, backgroundColor: "red", margin: 40 }} className='br-circle'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Grid></Grid>
+        </Grid>
+        */}
+        <Grid item><FontAwesomeIcon icon={faArrowAltCircleRight} style={{width: 80, height: 80}}/></Grid>
+      </Grid>
+    </Grid>
   );
 }
 
